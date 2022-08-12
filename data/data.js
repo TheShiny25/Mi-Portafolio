a37ce817f0ec7e4132dc7882e349a184a42dd79a
Gustavo Anchahua Taype <ganchahua@gmail.com>
2022-08-12 10:14:52 -0500

    Mi Portafolio - v1.0

---
+++ data/data.js
@@ -0,0 +1,85 @@
+const PROJECTS = [{
+    "id": "1",
+    "code": "https://github.com/ayauri29/LIM010-data-lovers",
+    "demo": "https://ayauri29.github.io/LIM010-data-lovers/src",
+    "title": "PokemonGO",
+    "description": "Plataforma web que presenta información reelevante del juego PokemonGo y estadísticas de los pokemones del juego.",
+    "tools": [
+        "JAVASCRIPT","HTML5","CSS3"
+    ],
+    "image": "https://user-images.githubusercontent.com/33959688/82073848-78455e80-969f-11ea-886b-e415231f2b51.png"
+},
+{
+    "id": "2",
+    "code": "https://github.com/ayauri29/LIM010-social-network",
+    "demo": "https://ayauri29.github.io/LIM010-social-network/src/",
+    "title": "MEET AND CODE",
+    "description": "Red social para personas interesadas en la tecnología, donde de pueden publicar eventos tecnológicos.",
+    "tools": ["JAVASCRIPT","CSS3","HTML5","FIREBASE"],
+    "image": "https://user-images.githubusercontent.com/33959688/82074029-be9abd80-969f-11ea-8e3a-39b856ddf370.png"
+},
+{
+    "id": "3",
+    "code": "https://github.com/ayauri29/LIM010-fe-md-links",
+    "demo": "https://www.npmjs.com/package/@ayauri29/md-links",
+    "title": "MD-LINKS",
+    "description": "Librería que brinda estadísticas sobre los links que se encuentren dentro de todos los archivos .md de una ruta dada.",
+    "tools": ["JAVASCRIPT", "NODEJS"],
+    "image": "https://user-images.githubusercontent.com/33959688/82074180-fbff4b00-969f-11ea-81a2-4050a457bd55.png"
+},
+{
+    "id": "4",
+    "code": "https://github.com/ayauri29/LIM010-Cipher",
+    "demo": "https://ayauri29.github.io/LIM010-Cipher/src/",
+    "title": "LOVE CIPHER",
+    "description": "Plataforma permite cifrar y descifrar mensajes de amor con una clave de seguridad, sin temor a ser leídas por terceros.",
+    "tools": ["JAVASCRIPT", "HTML5","CSS3"],
+    "image": "https://user-images.githubusercontent.com/33959688/82074337-3cf75f80-96a0-11ea-9259-5c79ab64bf4c.png"
+},
+{
+    "id": "5",
+    "code": "https://github.com/ayauri29/LIM010-hackathon-peliculas",
+    "demo": "https://ayauri29.github.io/LIM010-hackathon-peliculas/src/",
+    "title": "MOVIE LATIN",
+    "description": "Plataforma que muestra información de películas latinoamericanas y elegir qué ver en base a la puntuación de otros.",
+    "tools": ["JAVASCRIPT","HTML5","CSS3"],
+    "image": "https://user-images.githubusercontent.com/33959688/82074427-5e584b80-96a0-11ea-967f-07964ede273c.png"
+},
+{
+    "id": "6",
+    "code": "https://github.com/ayauri29/LIM010-fe-burger-queen-api",
+    "demo": "https://github.com/ayauri29/LIM010-fe-burger-queen-api",
+    "title": "BURGER QUEEN API",
+    "description": "Un servidor web debe manejar consultas entrantes y producir respuestas que serán enviadas de vuelta al cliente.",
+    "tools": ["JAVASCRIPT","NODEJS", "MONGODB"],
+    "image": "https://user-images.githubusercontent.com/33959688/68482506-bdc43300-0207-11ea-8a7a-7201102b0320.png"
+},
+{
+    "id": "7",
+    "code": "https://github.com/ayauri29/burger-queen",
+    "demo": "https://burger-queen-29.herokuapp.com/",
+    "title": "BURGER QUEEN",
+    "description": "Plataforma que muestra información el menú de un restaurante y permite añadir los productos  a un carrito de compra.",
+    "tools": ["VUE", "PUG","NUXT"],
+    "image": "https://user-images.githubusercontent.com/33959688/82074659-b8591100-96a0-11ea-837e-bd532c38a12a.png"
+},
+ {
+     "id": "8",
+     "code": "https://github.com/ayauri29/hackaton-users",
+     "demo": "https://ayauri29.github.io/hackaton-users/",
+     "title": "MAKEUP STORE",
+     "description": "Plataforma que muestra información de un catálogo de maquillaje y permite añadir los productos a un carrito de compra.",
+     "tools": ["REACT", "FIREBASE"],
+     "image": "../images/project/react.png"
+ },
+{
+    "id": "9",
+    "code": "https://github.com/ayauri29/tic-tac-toe/",
+    "demo": "https://ayauri29.github.io/tic-tac-toe/",
+    "title": "TIC-TAC-TOE",
+    "description": "Juego tres en raya o tic-tac-toe que permite ver los movimientos hechos.",
+    "tools": ["REACT"],
+    "image": "https://user-images.githubusercontent.com/33959688/82074772-e2123800-96a0-11ea-947a-6304c8aa815a.png"
+}
+
+]
